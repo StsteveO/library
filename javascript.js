5e1736db59ff8be5744e1b4fdf79a7667a6e8875
steve omari <steveomari62@gmail.com>
2023-01-13 23:59:38 -0500

    setup delete btn

--- javascript.js
+++ javascript.js
@@ -27,6 +27,7 @@ let author = document.getElementById("author");
 let pageNumbers = document.getElementById("page_numbers");
 let readStatus = document.getElementsByName("read_status"); //node list
 let readStatusArray=Array.from(readStatus);
+let main= document.getElementById("main");
 
 // EVENTS(WHAT TO DO)
 addBookBtn.addEventListener("click", () => {
@@ -46,6 +47,7 @@ form.addEventListener("submit", (e)=>{
   let newBook= new Book(title.value, author.value, pageNumbers.value, radioValue());
   myLibrary.push(newBook);
   closePopup();
+  pushBookToLibrary();
 });
 
 // FUNCTIONS
@@ -67,5 +69,54 @@ function radioValue() {
   }
 };
 
-//seeing myLibrary
-console.log(myLibrary);
+function pushBookToLibrary() {
+  for (let i = 0; i < 1; i++) {
+    let divCard= document.createElement("div");
+    divCard.classList.add("card");
+    divCard.setAttribute("id", `card-${myLibrary.length}`);
+    divCard.textContent=`Book Number: ${myLibrary.length}`;
+
+    main.appendChild(divCard);
+
+    let divCardTitle = document.createElement("div");
+    divCardTitle.classList.add("card-title");
+    divCardTitle.textContent = `Book Title: ${myLibrary[myLibrary.length-1].title}`;
+    divCard.appendChild(divCardTitle);
+
+    let divCardMainAuthor = document.createElement("div");
+    divCardMainAuthor.classList.add("card-main");
+    divCardMainAuthor.textContent = `Book Number: ${author.value}`;
+    divCard.appendChild(divCardMainAuthor);
+
+    let divCardMainPageNumber = document.createElement("div");
+    divCardMainPageNumber.classList.add("card-main");
+    divCardMainPageNumber.textContent = `Book Number: ${pageNumbers.value}`;
+    divCard.appendChild(divCardMainPageNumber);
+
+    let divCardFooter = document.createElement("div");
+    divCardFooter.classList.add("card-footer");
+    divCardFooter.textContent = `Book Number: ${radioValue()}`;
+    divCard.appendChild(divCardFooter);
+
+    let deleteBtn=document.createElement("button");
+    deleteBtn.textContent="Delete"
+    deleteBtn.classList.add(`${myLibrary.length}`);
+
+    deleteBtn.addEventListener("click", (e)=>{
+      let entry=(e.path[0]);
+      let entryNumber=(entry.getAttribute("class"));
+      let indexNumber=(entryNumber-1);
+      myLibrary.splice(indexNumber, 1);
+      divCard.classList.remove("card");
+      divCardTitle.classList.remove("card-title");
+      divCardMainAuthor.classList.remove("card-main");
+      divCardMainPageNumber.classList.remove("card-main");
+      divCardFooter.classList.add("card-footer");
+
+    });
+    divCard.appendChild(deleteBtn);
+  }
+}
+
+
+//delete= myLibrary.length-1
