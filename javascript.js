5424b18689364fe8302b52a2a82259f50fe24087
steve omari <steveomari62@gmail.com>
2023-01-29 19:00:49 -0500

    convert layout to classes and modules

--- javascript.js
+++ javascript.js
@@ -1,145 +1,148 @@
-let myLibrary = [];
+class Book{
 
-function Book(title, author, pages, haveRead) {
+  constructor (title, author, pages, haveRead) {
   this.title = title;
   this.author = author;
   this.pages = pages;
   this.haveRead = haveRead;
 }
+};
 
-function addBookToLibrary(usersInput) {
-  //take users input and stores new book object into myLibrary array
-}
+const connections=(()=>{
+  const addBookBtn = document.querySelector(".add-book-btn");
+  const form = document.querySelector(".form"); /////
+  const overlay = document.querySelector(".overlay"); /////
+  const closeFormBtn = document.querySelector(".close-btn");
+  const submitBtn = document.getElementById("submit-btn");
+  const title = document.getElementById("title"); //title.value= title
+  const author = document.getElementById("author");
+  const pageNumbers = document.getElementById("page_numbers");
+  const readStatus = document.getElementsByName("read_status"); //node list
+  const readStatusArray = Array.from(readStatus); /////
+  const main = document.getElementById("main"); /////
+  const cards = document.querySelectorAll("card");
 
-function bookPlacementLoop() {
-  //loops through the array and displays each book on the page
-}
-////////////////////////////////////////////////////////////////////////////////
-
-// CONNECTIONS
-let addBookBtn = document.querySelector(".add-book-btn");
-let form= document.querySelector(".form");
-let overlay= document.querySelector(".overlay");
-let closeFormBtn = document.querySelector(".close-btn");
-let submitBtn = document.getElementById("submit-btn");
-let title= document.getElementById("title"); //title.value= title
-let author = document.getElementById("author");
-let pageNumbers = document.getElementById("page_numbers");
-let readStatus = document.getElementsByName("read_status"); //node list
-let readStatusArray=Array.from(readStatus);
-let main= document.getElementById("main");
-let cards=document.querySelectorAll("card");
-
-// EVENTS(WHAT TO DO)
-addBookBtn.addEventListener("click", () => {
-  openPopup();
-});
-
-closeFormBtn.addEventListener("click", ()=>{
-  closePopup();
-});
-
-overlay.addEventListener("click", ()=>{
-  closePopup();  
-});
-
-form.addEventListener("submit", (e)=>{
-  e.preventDefault();
-  let newBook= new Book(title.value, author.value, pageNumbers.value, radioValue());
-  myLibrary.push(newBook);
-  closePopup();
-  pushBookToLibrary();
-});
+  return {form, overlay, readStatusArray, main, addBookBtn, closeFormBtn, pageNumbers};
+})();
 
 // FUNCTIONS
-function openPopup() {
-  form.classList.add("active");
-  overlay.classList.add("active");
-};
+const func=(()=>{
 
-function closePopup(){
-   form.classList.remove("active");
-   overlay.classList.remove("active");
-}
-
-function radioValue() {
-  if (readStatusArray[0].checked===true){
-    return readStatusArray[0].value;
-  }else{
-    return readStatusArray[1].value;
+  const openPopup=()=>{
+    (connections.form).classList.add("active");
+    (connections.overlay).classList.add("active");
   }
-};
 
-function pushBookToLibrary() {
-  for (let i = 0; i < 1; i++) {
-    let divCard= document.createElement("div");
-    divCard.classList.add("card");
-    if(radioValue()==="Read"){
-      divCard.classList.add("read");
-    }else{
-      divCard.classList.add("not-read");
-    };
-    divCard.setAttribute("id", `card-${myLibrary.length}`);
-    // divCard.textContent=`Book Number: ${myLibrary.length}`;
+  const closePopup=()=>{
+    (connections.form).classList.remove("active");
+    (connections.overlay).classList.remove("active");
+  }
 
-    main.appendChild(divCard);
+  const radioValue=()=>{
+    if ((connections.readStatusArray)[0].checked === true) {
+      return (connections.readStatusArray)[0].value;
+    } else {
+      return (connections.readStatusArray)[1].value;
+    }
+  }
 
-    let divCardTitle = document.createElement("div");
-    divCardTitle.classList.add("card-title");
-    divCardTitle.textContent = `${myLibrary[myLibrary.length-1].title}`;
-    divCard.appendChild(divCardTitle);
-
-    let divCardMainAuthor = document.createElement("div");
-    divCardMainAuthor.classList.add("card-main");
-    divCardMainAuthor.textContent = `By: ${author.value}`;
-    divCard.appendChild(divCardMainAuthor);
-
-    let divCardMainPageNumber = document.createElement("div");
-    divCardMainPageNumber.classList.add("card-main");
-    divCardMainPageNumber.textContent = `Page Numbers: ${pageNumbers.value}`;
-    divCard.appendChild(divCardMainPageNumber);
-
-    let divCardFooter = document.createElement("div");
-    divCardFooter.classList.add("card-footer");
-    divCardFooter.textContent = `Read, or Not Read? : ${radioValue()}`;
-    divCard.appendChild(divCardFooter);
-
-    let changeReadStatus = document.createElement("button");
-    changeReadStatus.textContent = "Change Read Status";
-    divCard.appendChild(changeReadStatus);
-    changeReadStatus.style.cssText= "width: 90%;";
-    changeReadStatus.style.cssText= "border-radius: .5rem;";
-    changeReadStatus.style.cssText= "padding: .5rem 1.5rem;";
-
-    changeReadStatus.addEventListener("click", (e)=>{
-      if ((e.path[1].getAttribute("class")) === "card read") {
-        e.path[1].setAttribute("class", "card not-read");
-        divCardFooter.textContent = `Read, or Not Read? : Not Read`;
-      } else if ((e.path[1].getAttribute("class")) === "card not-read") {
-        e.path[1].setAttribute("class", "card read");
-        divCardFooter.textContent = `Read, or Not Read? : Read`;
-      };
-    })
-
-    let deleteBtn=document.createElement("button");
-    deleteBtn.textContent="Delete"
-    deleteBtn.classList.add(`${myLibrary.length}`);
-    deleteBtn.style.cssText = "width: 90%;";
-    deleteBtn.style.cssText = "border-radius: .5rem;";
-    deleteBtn.style.cssText = "padding: .5rem 1.5rem;";
-
-    deleteBtn.addEventListener("click", (e)=>{
-      let text=("Are you sure you want to delete? \n\nThis action cannot be undone.");
-      if(confirm(text)===true){
-      let entry=(e.path[0]);
-      let entryNumber=(entry.getAttribute("class"));
-      let indexNumber=(entryNumber-1);
-      myLibrary.splice(indexNumber, 1);
-      divCard.remove();
-      }else{
-        return;
+  const pushBookToLibrary=()=>{
+    for (let i = 0; i < 1; i++) {
+      let divCard = document.createElement("div");
+      divCard.classList.add("card");
+      if (radioValue() === "Read") {
+        divCard.classList.add("read");
+      } else {
+        divCard.classList.add("not-read");
       }
-    });
-    divCard.appendChild(deleteBtn);
+      divCard.setAttribute("id", `card-${myLibrary.length}`);
+
+      (connections.main).appendChild(divCard);
+
+      let divCardTitle = document.createElement("div");
+      divCardTitle.classList.add("card-title");
+      divCardTitle.textContent = `${myLibrary[myLibrary.length - 1].title}`;
+      divCard.appendChild(divCardTitle);
+
+      let divCardMainAuthor = document.createElement("div");
+      divCardMainAuthor.classList.add("card-main");
+      divCardMainAuthor.textContent = `By: ${author.value}`;
+      divCard.appendChild(divCardMainAuthor);
+
+      let divCardMainPageNumber = document.createElement("div");
+      divCardMainPageNumber.classList.add("card-main");
+      divCardMainPageNumber.textContent = `Page Numbers: ${(connections.pageNumbers).value}`;
+      divCard.appendChild(divCardMainPageNumber);
+
+      let divCardFooter = document.createElement("div");
+      divCardFooter.classList.add("card-footer");
+      divCardFooter.textContent = `Read, or Not Read? : ${radioValue()}`;
+      divCard.appendChild(divCardFooter);
+
+      let changeReadStatus = document.createElement("button");
+      changeReadStatus.textContent = "Change Read Status";
+      divCard.appendChild(changeReadStatus);
+      changeReadStatus.style.cssText = "width: 90%;";
+      changeReadStatus.style.cssText = "border-radius: .5rem;";
+      changeReadStatus.style.cssText = "padding: .5rem 1.5rem;";
+
+      changeReadStatus.addEventListener("click", (e) => {
+        if (e.path[1].getAttribute("class") === "card read") {
+          e.path[1].setAttribute("class", "card not-read");
+          divCardFooter.textContent = `Read, or Not Read? : Not Read`;
+        } else if (e.path[1].getAttribute("class") === "card not-read") {
+          e.path[1].setAttribute("class", "card read");
+          divCardFooter.textContent = `Read, or Not Read? : Read`;
+        }
+      });
+
+      let deleteBtn = document.createElement("button");
+      deleteBtn.textContent = "Delete";
+      deleteBtn.classList.add(`${myLibrary.length}`);
+      deleteBtn.style.cssText = "width: 90%;";
+      deleteBtn.style.cssText = "border-radius: .5rem;";
+      deleteBtn.style.cssText = "padding: .5rem 1.5rem;";
+
+      deleteBtn.addEventListener("click", (e) => {
+        let text =
+          "Are you sure you want to delete? \n\nThis action cannot be undone.";
+        if (confirm(text) === true) {
+          let entry = e.path[0];
+          let entryNumber = entry.getAttribute("class");
+          let indexNumber = entryNumber - 1;
+          myLibrary.splice(indexNumber, 1);
+          divCard.remove();
+        } else {
+          return;
+        }
+      });
+      divCard.appendChild(deleteBtn);
+    }
   }
-}
+  return{openPopup, closePopup, pushBookToLibrary, radioValue};
+})();
+
+// EVENTS(WHAT TO DO)
+const event= (()=>{
+(connections.addBookBtn).addEventListener("click", () => {
+  func.openPopup();
+});
+
+(connections.closeFormBtn).addEventListener("click", ()=>{
+  func.closePopup();
+});
+
+(connections.overlay).addEventListener("click", ()=>{
+  func.closePopup();  
+});
+
+(connections.form).addEventListener("submit", (e)=>{
+  myLibrary = [];
+  e.preventDefault();
+  const newBook= new Book(title.value, author.value, (connections.pageNumbers).value, func.radioValue());
+  myLibrary.push(newBook);
+  func.closePopup();
+  func.pushBookToLibrary();
+});
+return {};
+})();
